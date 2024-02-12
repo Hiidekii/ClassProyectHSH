4cc8525af855e29072f134220972a86e49d6ff6c
HidekiS <20212604@aloe.ulima.edu.pe>
2024-02-11 22:22:26 -0500

    solo visual

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import LoginPage from './presentation/login/LoginPage'
 import MainPage from './presentation/main/MainPage'
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <LoginPage />
-    },
-    {
-        path: "/main",
-        element: <MainPage />
-    }
-])
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <LoginPage />
     </React.StrictMode>
 )
