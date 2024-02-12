02edb643471f5e50354d04da93ee14251b2b1093
HidekiS <20212604@aloe.ulima.edu.pe>
2024-02-11 23:44:23 -0500

    navegacion hecha

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import LoginPage from './presentation/login/LoginPage'
 import MainPage from './presentation/main/MainPage'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <LoginPage />
+    },
+    {
+        path: "/main",
+        element: <MainPage />
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <LoginPage />
+        <RouterProvider router={router} />
     </React.StrictMode>
 )
